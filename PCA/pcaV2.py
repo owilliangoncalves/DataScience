af5aeee66dc7dcc5e7be14ecd5cb60b40c0c5250
Willian Goncalves <williangoncalves74@gmail.com>
2024-08-30 16:50:14 -0300

    criado versao 2 do PCA e separado as funcoes da execucao das funcoes

--- PCA/pcaV2.py
+++ PCA/pcaV2.py
@@ -1,13 +1,17 @@
 #Import dos pacotes necessários
 import numpy as np
-import pandas as pd
-
-#Importando o dataset
-df = pd.read_csv("PCA/dataset.csv", index_col=0)
-print(df.shape) #repare que o df possui mais variáveis do que exemplos, ou seja, mais colunas do que linha e isso é um problema para um bom modelo de machine learning
-
-#Convertendo os dados para um array numpy
-matriz = df.to_numpy()
 
 def pcaV2 (input):
-  
+  mean = np.mean(input, axis=0) #Calculando a média de cada coluna
+  normalisedInput = input - mean #Subtraindo a média da matriz de entrada 
+  covMatriz = np.cov(normalisedInput, rowvar=False) #Calculando a matriz de covariância
+  eigenvalues, eigenvectors, = np.linalg.eigh(covMatriz) #Localizando autovalores e autovetores utilizando uma função mais adequada (eigh) para matrizes simétricas
+
+  #Ordenando os AUTOVETORES de acordo com os AUTOVALORES em ordem decrescente
+  sortedIndices = np.argsort(eigenvalues) [::-1]
+  sortedEigenValues = eigenvalues[sortedIndices]
+  sortedEingenVectors = eigenvectors[:, sortedIndices]
+
+  pcaOutput = np.dot(normalisedInput, sortedEingenVectors)
+
+  return pcaOutput, sortedEigenValues, sortedEingenVectors, covMatriz
