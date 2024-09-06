5212e22d726c7a1cc0275b27229f4768bc9ff43d
Willian Goncalves <williangoncalves74@gmail.com>
2024-09-06 08:27:53 -0300

    ajustado requirements com matplotlib

--- transformer/imports.py
+++ transformer/imports.py
@@ -1,26 +1,23 @@
-# Bibliotecas de machine learning
+
+import evaluate
 import torch
 import transformers
-from transformers import TimeSeriesTransformerConfig as TSTConfig
-from transformers import TimeSeriesTransformerForPrediction as TSTForPrediction
-from gluonts import *
-
-# Bibliotecas de visualização
-import matplotlib.pyplot as plt # type: ignore
-import matplotlib.dates as mdates # type: ignore
-
-# Bibliotecas de manipulação de dados
+import accelerate
+import gluonts
 import pandas as pd
 import numpy as np
 from datasets import load_dataset
-
-# Outros imports
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from functools import lru_cache
 from functools import partial
+from transformers import TimeSeriesTransformerConfig, TimeSeriesTransformerForPrediction
+from transformers import PretrainedConfig
 from typing import Optional
 from accelerate import Accelerator
 from torch.optim import AdamW
 from evaluate import load
+from typing import Iterable
 from gluonts.itertools import Cached, Cyclic
 from gluonts.dataset.loader import as_stacked_batches
 from gluonts.time_feature import get_seasonality
@@ -46,7 +43,5 @@ from gluonts.transform import (
     VstackFeatures,
     RenameFields,
 )
-
-# Desativa warnings
 import warnings
 warnings.filterwarnings('ignore')
