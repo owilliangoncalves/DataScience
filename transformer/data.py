4f3d71305ff8988e91c81039287f9c39557a43c8
Willian Goncalves <williangoncalves74@gmail.com>
2024-09-06 09:40:19 -0300

    criado a função de conversão de datas para períodos e a entrega desses dados em pacote para o modelo

--- transformer/data.py
+++ transformer/data.py
@@ -4,3 +4,5 @@ def data ():
   """"
   Essa função realiza o carregamento do dataset que será utilizado. Nesse exemplo, o repositório monash_tsf com dataset tourism_monthly disponível no Hugging Face."""
   dataset = load_dataset("monash_tsf", "tourism_monthly")
+  treino = dataset["train"]
+  teste = dataset["test"]
--- transformer/main.py
+++ transformer/main.py
@@ -1,12 +1,25 @@
-from imports import imports
-from data import data
+from imports import *
+from data import *
 
 def main ():
   """
   Função principal que inicializa as demais funções.
   """
+  def init():
+    imports()
+    data()
+  init()
+# Converte data para períodos de datas
+def convertPandasPeriod(date,freq):
+  return pd.Period(date, freq)
 
-  imports()
-  data()
+#Inicio de batch de dados
+def start(batch, freq):
+  """"
+  O batch de dados deve ser feito pois modelos de deep learning necessitam que os dados sejam entregues em "pacotes" devido recurso de memória
+"""
+  batch["start"] = [convertPandasPeriod(date, freq) for date in batch["start"]]
+  return batch
+
 if __name__ == "__main__":
   main()
